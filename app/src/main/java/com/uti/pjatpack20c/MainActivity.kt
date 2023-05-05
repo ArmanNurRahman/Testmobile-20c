2981a70f3ebbf797ca8fc3af9824b74a5945b403
ArmanNR25 <arman_nur_rahman@teknokrat.ac.id>
2023-05-05 20:33:22 +0700

    Ubah Hello menjadi woy

--- app/src/main/java/com/uti/pjatpack20c/MainActivity.kt
+++ app/src/main/java/com/uti/pjatpack20c/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Woygi $name!")
 }
 
 @Preview(showBackground = true)
